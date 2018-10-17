8b5f3599d17452e396d9e1d996dacbe9575aba40
fulrich <9284621+fulrich@users.noreply.github.com>
2018-10-17 12:51:51 -0400

    updated site

--- src/main/scala/com/github/fulrich/testcharged/generators/locations/LocationGenerators.scala
+++
@@ -1,7 +0,0 @@
-package com.github.fulrich.testcharged.generators.locations
-
-
-trait LocationGenerators {
-  val street = StreetGenerators
-  val country = CountryGenerators
-}
--- src/main/scala/com/github/fulrich/testcharged/generators/strings/StringGenerators.scala
+++
@@ -1,9 +0,0 @@
-package com.github.fulrich.testcharged.generators.strings
-
-import com.github.fulrich.testcharged.generators.DefaultSizeGenerator
-
-
-trait StringGenerators {
-  val alpha: DefaultSizeGenerator[String] = AlphaGenerators
-  val alphaNumeric: DefaultSizeGenerator[String] = AlphaNumericGenerators
-}
