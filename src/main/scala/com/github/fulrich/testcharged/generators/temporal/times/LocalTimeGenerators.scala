db64562cfe885e1c80e024ffecaa56c758fd1f10
Fred Ulrich <9284621+fulrich@users.noreply.github.com>
2018-12-20 15:56:54 -0500

    Updating LocalTimeGenerators for Java 8. Adding Documentation menus.

--- src/main/scala/com/github/fulrich/testcharged/generators/temporal/times/LocalTimeGenerators.scala
+++ src/main/scala/com/github/fulrich/testcharged/generators/temporal/times/LocalTimeGenerators.scala
@@ -11,11 +11,12 @@ object LocalTimeGenerators extends TemporalGenerator[LocalTime] with TimeRanges 
   private type Edge = LocalTime => LocalTime
   private val Default: Edge = identity[LocalTime]
 
+  private val LocalDateEpoch = Instant.EPOCH.atZone(ZoneOffset.UTC).toLocalDate
 
   def apply(min: Edge = Default, max: Edge = Default)(implicit now: NowProvider): Gen[LocalTime] = for {
     createdInstant <- InstantGenerators(
-      Instant.ofEpochSecond(min(now.localTime).atDate(LocalDate.EPOCH).atZone(ZoneOffset.UTC).toEpochSecond),
-      Instant.ofEpochSecond(max(now.localTime).atDate(LocalDate.EPOCH).atZone(ZoneOffset.UTC).toEpochSecond)
+      Instant.ofEpochSecond(min(now.localTime).atDate(LocalDateEpoch).atZone(ZoneOffset.UTC).toEpochSecond),
+      Instant.ofEpochSecond(max(now.localTime).atDate(LocalDateEpoch).atZone(ZoneOffset.UTC).toEpochSecond)
     )
   } yield LocalDateTime.ofInstant(createdInstant, ZoneOffset.UTC).toLocalTime
 }
