45b31524a85c6fcce8896f1a7b0167af6677eb38
Fred Ulrich <9284621+fulrich@users.noreply.github.com>
2018-12-20 16:02:11 -0500

    Updating tests for Java 8 compatability.

--- src/test/scala/com/github/fulrich/testcharged/generators/temporal/NowDslUTest.scala
+++ src/test/scala/com/github/fulrich/testcharged/generators/temporal/NowDslUTest.scala
@@ -6,24 +6,26 @@ import org.scalatest.{FunSuite, Matchers}
 
 
 class NowDslUTest extends FunSuite with Matchers with NowDsl {
+  private val LocalDateEpoch = Instant.EPOCH.atZone(ZoneOffset.UTC).toLocalDate
+  private val LocalTimeEpoch = Instant.EPOCH.atZone(ZoneOffset.UTC).toLocalTime
 
   test("Can generate a NowProvider from an Instant") {
     val nowProvider = now(Instant.EPOCH)
 
-    nowProvider.localDate shouldBe LocalDate.EPOCH
-    nowProvider.localTime shouldBe LocalTime.MIDNIGHT
+    nowProvider.localDate shouldBe LocalDateEpoch
+    nowProvider.localTime shouldBe LocalTimeEpoch
     nowProvider.instant shouldBe Instant.EPOCH
-    nowProvider.localDateTime shouldBe LocalDateTime.of(LocalDate.EPOCH, LocalTime.MIDNIGHT)
+    nowProvider.localDateTime shouldBe LocalDateTime.of(LocalDateEpoch, LocalTimeEpoch)
     nowProvider.zone shouldBe ZoneOffset.UTC
   }
 
   test("Can generate a NowProvider from an LocalDateTime") {
-    val nowProvider = now(LocalDateTime.of(LocalDate.EPOCH, LocalTime.MIDNIGHT))
+    val nowProvider = now(LocalDateTime.of(LocalDateEpoch, LocalTimeEpoch))
 
-    nowProvider.localDate shouldBe LocalDate.EPOCH
-    nowProvider.localTime shouldBe LocalTime.MIDNIGHT
+    nowProvider.localDate shouldBe LocalDateEpoch
+    nowProvider.localTime shouldBe LocalTimeEpoch
     nowProvider.instant shouldBe Instant.EPOCH
-    nowProvider.localDateTime shouldBe LocalDateTime.of(LocalDate.EPOCH, LocalTime.MIDNIGHT)
+    nowProvider.localDateTime shouldBe LocalDateTime.of(LocalDateEpoch, LocalTime.MIDNIGHT)
     nowProvider.zone shouldBe ZoneOffset.UTC
   }
 }
