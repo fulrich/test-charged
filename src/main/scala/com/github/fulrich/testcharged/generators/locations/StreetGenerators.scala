ea9391c667a74c1785a5b3e4e61e86c5aac7fd83
Vlad Mandrychenko <vlad.mandrychenko@garnercorp.com>
2019-05-28 11:30:21 -0400

    Fixing issue #9
     - removing formatting

--- src/main/scala/com/github/fulrich/testcharged/generators/locations/StreetGenerators.scala
+++ src/main/scala/com/github/fulrich/testcharged/generators/locations/StreetGenerators.scala
@@ -6,20 +6,22 @@ import org.scalacheck.Gen
 
 import scala.io.Source
 
+
 object StreetGenerators {
   private lazy val streetFile = "/generators/locations/streets.txt"
   private lazy val suffixesFile = "/generators/locations/street-suffixes.txt"
-  
+
   lazy val Names: Seq[String] = Source.fromInputStream(getClass.getResourceAsStream(streetFile)).getLines.toVector
   lazy val Suffixes: Seq[String] = Source.fromInputStream(getClass.getResourceAsStream(suffixesFile)).getLines.toVector
-  
+
   val MinimumStreetNumber = 1
   val MaximumStreetNumber = 9999
-  
+
+
   val number: Gen[Int] = IntGenerators(MinimumStreetNumber, MaximumStreetNumber)
   val name: Gen[String] = Gen.oneOf(Names)
   val suffix: Gen[String] = Gen.oneOf(Suffixes)
-  
+
   val default: Gen[String] = for {
     generatedStreetNumber <- number
     generatedStreetName <- name
